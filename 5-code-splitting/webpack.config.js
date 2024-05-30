2906da074b43aa6aa0deaebda502bc32777521d8
Cédric RIBALTA <cedric.ribalta@assurant.com>
2024-05-30 17:35:08 +0200

    Initial commit

---
+++ 5-code-splitting/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    another: "./src/another-module.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Code splitting",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+};
